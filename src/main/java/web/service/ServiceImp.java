a3537b6c81e92481cb0e29a6a0ced1e7b37b65cf
Olga Bystrova <bystrovaolya@gmail.com>
2022-08-11 00:13:02 +0300

    Initial commit

---
+++ src/main/java/web/service/ServiceImp.java
@@ -0,0 +1,31 @@
+package web.service;
+
+import org.springframework.stereotype.Component;
+import web.model.Car;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class ServiceImp implements Service {
+
+    @Override
+    public List<Car> getCarsList(int count) {
+
+        List<Car> cars = new ArrayList<>();
+
+        Car car1 = new Car("Audi", "Black", 2021);
+        Car car2 = new Car("BMW", "White", 2020);
+        Car car3 = new Car("Porsche", "Red", 2019);
+        Car car4 = new Car("Ford", "Green", 2018);
+        Car car5 = new Car("Infiniti", "Blue", 2017);
+
+        cars.add(car1);
+        cars.add(car2);
+        cars.add(car3);
+        cars.add(car4);
+        cars.add(car5);
+
+        return cars.stream().limit(count).collect(Collectors.toList());
+    }
+}
