08096fdbcf54d93c67397bfac5e97f2e40cc5685
Olga Bystrova <bystrovaolya@gmail.com>
2022-08-11 10:16:54 +0300

    Update

--- src/main/java/web/model/Car.java
+++ src/main/java/web/model/Car.java
@@ -13,9 +13,13 @@ public class Car {
         this.model = model;
     }
 
-    public String getColour() { return colour; }
+    public String getColour() {
+        return colour;
+    }
 
-    public void setColour(String colour) { this.colour = colour; }
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
 
     public int getProdYear() {
         return prodYear;
